e7adcb7e16af6f4db0f4a391b1e32c3d99134ab3
reich <markus.reich@egger.com>
2015-02-03 14:41:50 +0100

    Added SQL Dump

---
+++ docs/crosswords.sql
@@ -0,0 +1,60 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.05 
+MySQL - 5.6.16 : Database - crosswords
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`crosswords` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `crosswords`;
+
+/*Table structure for table `term` */
+
+DROP TABLE IF EXISTS `term`;
+
+CREATE TABLE `term` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `term` varchar(255) NOT NULL,
+  `token_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_pqses8vpcx79r4kgnx9s6mohh` (`token_id`),
+  CONSTRAINT `FK_pqses8vpcx79r4kgnx9s6mohh` FOREIGN KEY (`token_id`) REFERENCES `token` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31972 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `token` */
+
+DROP TABLE IF EXISTS `token`;
+
+CREATE TABLE `token` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_pddrhgwxnms2aceeku9s2ewy5` (`token`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_i3e9qhkr9wead1gstwc34yimg` (`user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
